b1d81065a97a1d4ea89e832eb8cc87ed4c4d463e
Yossi Kolesnicov <yoxigen@gmail.com>
2023-11-15 16:45:41 +0200

    making it work

--- scripts/list.js
+++ scripts/list.js
@@ -1,16 +1,32 @@
 const list = [
-    {
-        name: 'Joe Biden',
-        country: 'USA',
-        denounced: true,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Joe_Biden_presidential_portrait.jpg/640px-Joe_Biden_presidential_portrait.jpg'
-    },
-    {
-        name: 'Vladimir Putin',
-        country: 'Russia',
-        denounced: false,
-        image: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/rockcms/2022-11/221109-vladimir-putin-se-1143p-4c7bd0.jpg'
-    }    
+  {
+    name: "Joe Biden",
+    country: "United States",
+    denounced: true,
+    image:
+      "https://d3g9pb5nvr3u7.cloudfront.net/authors/59bb2de59744155625a7c141/129262217/256.jpg",
+  },
+  {
+    name: "Vladimir Putin",
+    country: "Russia",
+    denounced: false,
+    image:
+      "https://img.thedailybeast.com/image/upload/dpr_2.0/c_crop,h_2266,w_2266,x_1332,y_1874/c_limit,w_128/d_placeholder_euli9k,fl_lossy,q_auto/v1680605066/2023-04-04T093239Z_484216207_RC2L70A8EKWV_RTRMADP_3_RUSSIA-PUTIN_rmqnch",
+  },
+  {
+    name: "Lula da Silva",
+    country: "Brazil",
+    denounced: false,
+    image:
+      "https://i0.wp.com/raiagroup.org/wp-content/uploads/2021/03/Luiz-Inacio-Lula-da-Silva.jpg?resize=256%2C256&ssl=1",
+  },
+  {
+    name: "Alexander De Croo",
+    country: "Belgium",
+    denounced: false,
+    image:
+      "https://nu.cw/wp-content/uploads/2023/07/politie-zoekt-ex-militair-die-belgische-premier-online-met-dood-bedreigde.jpg",
+  },
 ];
 
 export default list;
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,30 @@
-import list from './list.js';
+import list from "./list.js";
 
 function main() {
-    const containerEl = document.querySelector("#container");
+  const containerEl = document.querySelector("#leaders");
 
-    const fragment = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment();
 
-    list.forEach(leader => {
-        const listItem = document.createElement('li');
-        const image = document.createElement('img');
-        image.src = leader.image;
-        image.setAttribute('alt', leader.name);
-        listItem.appendChild(image);
-        fragment.appendChild(listItem);
-    });
+  list.forEach((leader) => {
+    const listItem = document.createElement("li");
+    listItem.style.backgroundImage = `url(${leader.image})`;
+    listItem.className = "leader";
 
-    containerEl.appendChild(fragment);
+    listItem.setAttribute("title", `${leader.name} (${leader.country})`);
+    if (leader.denounced) {
+      listItem.dataset.denounced = "1";
+    }
+
+    const leaderName = document.createElement("span");
+    leaderName.innerHTML = `${leader.name}<br />(${leader.country})`;
+    leaderName.className = "leader-name";
+
+    listItem.appendChild(leaderName);
+    fragment.appendChild(listItem);
+  });
+
+  containerEl.appendChild(fragment);
 }
 
 main();
+N;
