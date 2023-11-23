7cfe7afb6f31527a5c5cf16558f7dda51c0b9855
Yossi Kolesnicov <yoxigen@gmail.com>
2023-11-23 17:00:41 +0200

    logos

--- scripts/list.js
+++ scripts/list.js
@@ -1,88 +1,350 @@
 const list = [
   {
-    name: "Joe Biden",
-    country: "United States",
-    denounced: true,
-    image:
-      "https://d3g9pb5nvr3u7.cloudfront.net/authors/59bb2de59744155625a7c141/129262217/256.jpg",
+    name: "ale_logo copy",
+    denounced: false,
+    image: "ale_logo%20copy.png",
   },
   {
-    name: "Vladimir Putin",
-    country: "Russia",
+    name: "ambridge - logo",
+    denounced: false,
+    image: "ambridge%20-%20logo.png",
+  },
+  {
+    name: "are international",
+    denounced: false,
+    image: "are%20international.png",
+  },
+  {
+    name: "arvard_university_logo.svg copy",
+    denounced: false,
+    image: "arvard_university_logo.svg%20copy.png",
+  },
+  {
+    name: "atholic relief services 2",
+    denounced: false,
+    image: "atholic%20relief%20services%202.png",
+  },
+  {
+    name: "AVE THE CHILDREN 3",
+    denounced: false,
+    image: "AVE%20THE%20CHILDREN%203.png",
+  },
+  {
+    name: "awcett society uk1",
+    denounced: false,
+    image: "awcett%20society%20uk1.png",
+  },
+  {
+    name: "AWID2",
+    denounced: false,
+    image: "AWID2.png",
+  },
+  {
+    name: "cl - logo",
+    denounced: false,
+    image: "cl%20-%20logo.png",
+  },
+  {
+    name: "CTION AID 2",
+    denounced: false,
+    image: "CTION%20AID%202.png",
+  },
+  {
+    name: "e7675f42f713055b49595da18fcfca42",
+    denounced: false,
+    image: "e7675f42f713055b49595da18fcfca42.png",
+  },
+  {
+    name: "EE Network for Gender Issues",
+    denounced: false,
+    image: "EE%20Network%20for%20Gender%20Issues.png",
+  },
+  {
+    name: "eministeerium",
+    denounced: false,
+    image: "eministeerium.png",
+  },
+  {
+    name: "enter for Reproductive Rights",
+    denounced: false,
+    image: "enter%20for%20Reproductive%20Rights.png",
+  },
+  {
+    name: "ings college london - logo_",
+    denounced: false,
+    image: "ings%20college%20london%20-%20logo_.png",
+  },
+  {
+    name: "ise Up - Logo",
+    denounced: false,
+    image: "ise%20Up%20-%20Logo.png",
+  },
+  {
+    name: "isters uncut",
+    denounced: false,
+    image: "isters%20uncut.png",
+  },
+  {
+    name: "it_logo8 copy",
+    denounced: false,
+    image: "it_logo8%20copy.png",
+  },
+  {
+    name: "iversity-of-southern-california-logo",
+    denounced: false,
+    image: "iversity-of-southern-california-logo.png",
+  },
+  {
+    name: "l-algemeen-internationaal-rgb-color",
+    denounced: false,
+    image: "l-algemeen-internationaal-rgb-color.png",
+  },
+  {
+    name: "lan-international",
+    denounced: false,
+    image: "lan-international.png",
+  },
+  {
+    name: "LOBAL FUND FOR WOMEN",
+    denounced: false,
+    image: "LOBAL%20FUND%20FOR%20WOMEN.png",
+  },
+  {
+    name: "mnesty-candle -3",
+    denounced: false,
+    image: "mnesty-candle%20-3.png",
+  },
+  {
+    name: "mperial college london - logo_",
+    denounced: false,
+    image: "mperial%20college%20london%20-%20logo_.png",
+  },
+  {
+    name: "n_women",
+    denounced: false,
+    image: "n_women.png",
+  },
+  {
+    name: "NFPA",
+    denounced: false,
+    image: "NFPA.png",
+  },
+  {
+    name: "nicef 3",
+    denounced: false,
+    image: "nicef%203.png",
+  },
+  {
+    name: "niversity-of-maryland-Seal-01 copy",
+    denounced: false,
+    image: "niversity-of-maryland-Seal-01%20copy.png",
+  },
+  {
+    name: "niversity-of-pennsylvania-seeklogo.com",
+    denounced: false,
+    image: "niversity-of-pennsylvania-seeklogo.com.png",
+  },
+  {
+    name: "niversity_of_california_Berkeley",
+    denounced: false,
+    image: "niversity_of_california_Berkeley.png",
+  },
+  {
+    name: "niversity_of_south_carolina_logo_copy",
+    denounced: false,
+    image: "niversity_of_south_carolina_logo_copy.png",
+  },
+  {
+    name: "nternational alliance of women",
+    denounced: false,
+    image: "nternational%20alliance%20of%20women.png",
+  },
+  {
+    name: "nternational Center for Research on Women 2",
+    denounced: false,
+    image: "nternational%20Center%20for%20Research%20on%20Women%202.png",
+  },
+  {
+    name: "nternational Women_s Health Coalition IWHC 1",
+    denounced: false,
+    image: "nternational%20Women_s%20Health%20Coalition%20IWHC%201.png",
+  },
+  {
+    name: "obel_women_s",
+    denounced: false,
+    image: "obel_women_s.png",
+  },
+  {
+    name: "ogo_lmu_og",
+    denounced: false,
+    image: "ogo_lmu_og.png",
+  },
+  {
+    name: "omankind Worldwide - logo",
+    denounced: false,
+    image: "omankind%20Worldwide%20-%20logo.png",
+  },
+  {
+    name: "omen deliver 1",
+    denounced: false,
+    image: "omen%20deliver%201.png",
+  },
+  {
+    name: "omen for Women International-5",
+    denounced: false,
+    image: "omen%20for%20Women%20International-5.png",
+  },
+  {
+    name: "omen_s International League for Peace and Freedom 3",
     denounced: false,
     image:
-      "https://img.thedailybeast.com/image/upload/dpr_2.0/c_crop,h_2266,w_2266,x_1332,y_1874/c_limit,w_128/d_placeholder_euli9k,fl_lossy,q_auto/v1680605066/2023-04-04T093239Z_484216207_RC2L70A8EKWV_RTRMADP_3_RUSSIA-PUTIN_rmqnch",
+      "omen_s%20International%20League%20for%20Peace%20and%20Freedom%203.png",
   },
   {
-    name: "Lula da Silva",
-    country: "Brazil",
+    name: "omen_s lobby",
+    denounced: false,
+    image: "omen_s%20lobby.png",
+  },
+  {
+    name: "ORLD YWCA 1",
+    denounced: false,
+    image: "ORLD%20YWCA%201.png",
+  },
+  {
+    name: "ORLED VISION CHILDREN",
+    denounced: false,
+    image: "ORLED%20VISION%20CHILDREN.png",
+  },
+  {
+    name: "outhall black sister 2",
+    denounced: false,
+    image: "outhall%20black%20sister%202.png",
+  },
+  {
+    name: "outhall black sister 3",
+    denounced: false,
+    image: "outhall%20black%20sister%203.png",
+  },
+  {
+    name: "OW",
+    denounced: false,
+    image: "OW.png",
+  },
+  {
+    name: "pfl - logo_",
+    denounced: false,
+    image: "pfl%20-%20logo_.png",
+  },
+  {
+    name: "quality Now",
+    denounced: false,
+    image: "quality%20Now.png",
+  },
+  {
+    name: "quality-fund1",
+    denounced: false,
+    image: "quality-fund1.png",
+  },
+  {
+    name: "Rainn",
+    denounced: false,
+    image: "rainn.png",
+  },
+  {
+    name: "rinceton_university-Logo.wine copy",
+    denounced: false,
+    image: "rinceton_university-Logo.wine%20copy.png",
+  },
+  {
+    name: "ronduked copy",
+    denounced: false,
+    image: "ronduked%20copy.png",
+  },
+  {
+    name: "shr-logo",
+    denounced: false,
+    image: "shr-logo.png",
+  },
+  {
+    name: "ssociation for the prevention and handling of violence in the Family",
     denounced: false,
     image:
-      "https://i0.wp.com/raiagroup.org/wp-content/uploads/2021/03/Luiz-Inacio-Lula-da-Silva.jpg?resize=256%2C256&ssl=1",
+      "ssociation%20for%20the%20prevention%20and%20handling%20of%20violence%20in%20the%20Family.png",
   },
   {
-    name: "Emmanuel Macron",
-    country: "France",
-    denounced: true,
-    image: "img/orgs/france.jpg",
-  },
-  {
-    name: "Alexander De Croo",
-    country: "Belgium",
+    name: "ssociazione italiana donne per lo sviluppo",
     denounced: false,
-    image:
-      "https://nu.cw/wp-content/uploads/2023/07/politie-zoekt-ex-militair-die-belgische-premier-online-met-dood-bedreigde.jpg",
+    image: "ssociazione%20italiana%20donne%20per%20lo%20sviluppo.png",
   },
   {
-    name: "Nikos Christodoulides",
-    country: "Cyprus",
-    denounced: true,
-    image: "img/orgs/cyprus.jpg",
-  },
-  {
-    name: "Olaf Scholz",
-    country: "Germany",
-    denounced: true,
-    image: "img/orgs/germany.jpg",
-  },
-  {
-    name: "Xi Jinping",
-    country: "China",
+    name: "th - logo",
     denounced: false,
-    image:
-      "https://styles.redditmedia.com/t5_3wpa92/styles/profileIcon_udclb3k9yid71.jpg?width=256&height=256&frame=1&auto=webp&crop=256:256,smart&s=d1d5b53ff8072192aadb7b983c0c6da895e7a50d",
+    image: "th%20-%20logo.png",
   },
   {
-    name: "Emmanuel Macron",
-    country: "France",
-    denounced: true,
-    image: "img/orgs/france.jpg",
-  },
-  {
-    name: "Alexander De Croo",
-    country: "Belgium",
+    name: "The Bulgarian Fund for Women",
     denounced: false,
-    image:
-      "https://nu.cw/wp-content/uploads/2023/07/politie-zoekt-ex-militair-die-belgische-premier-online-met-dood-bedreigde.jpg",
+    image: "The%20Bulgarian%20Fund%20for%20Women.png",
   },
   {
-    name: "Nikos Christodoulides",
-    country: "Cyprus",
-    denounced: true,
-    image: "img/orgs/cyprus.jpg",
-  },
-  {
-    name: "Olaf Scholz",
-    country: "Germany",
-    denounced: true,
-    image: "img/orgs/germany.jpg",
-  },
-  {
-    name: "Xi Jinping",
-    country: "China",
+    name: "The Feminist Association Union",
     denounced: false,
-    image:
-      "https://styles.redditmedia.com/t5_3wpa92/styles/profileIcon_udclb3k9yid71.jpg?width=256&height=256&frame=1&auto=webp&crop=256:256,smart&s=d1d5b53ff8072192aadb7b983c0c6da895e7a50d",
+    image: "The%20Feminist%20Association%20Union.png",
+  },
+  {
+    name: "uke_university_logo copy",
+    denounced: false,
+    image: "uke_university_logo%20copy.png",
+  },
+  {
+    name: "um - logo_",
+    denounced: false,
+    image: "um%20-%20logo_.png",
+  },
+  {
+    name: "uman Rights Watch",
+    denounced: false,
+    image: "uman%20Rights%20Watch.png",
+  },
+  {
+    name: "vaw coaliation",
+    denounced: false,
+    image: "vaw%20coaliation.png",
+  },
+  {
+    name: "wda3",
+    denounced: false,
+    image: "wda3.png",
+  },
+  {
+    name: "WID",
+    denounced: false,
+    image: "WID.png",
+  },
+  {
+    name: "Women International GWI",
+    denounced: false,
+    image: "Women%20International%20GWI.png",
+  },
+  {
+    name: "wsagreen 1",
+    denounced: false,
+    image: "wsagreen%201.png",
+  },
+  {
+    name: "x-yale_university_logo copy",
+    denounced: false,
+    image: "x-yale_university_logo%20copy.png",
+  },
+  {
+    name: "xford - logo",
+    denounced: false,
+    image: "xford%20-%20logo.png",
+  },
+  {
+    name: "zech Women_s Lobby",
+    denounced: false,
+    image: "zech%20Women_s%20Lobby.png",
   },
 ];
 
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 import list from "./list.js";
 import { setCountdown } from "./countdown.js";
 
+const LOGOS_FOLDER = "img/logos/";
+
 function main() {
   const containerEl = document.querySelector("#orgs");
 
@@ -8,16 +10,16 @@ function main() {
 
   list.forEach((org) => {
     const listItem = document.createElement("li");
-    listItem.style.backgroundImage = `url(${org.image})`;
+    listItem.style.backgroundImage = `url(${LOGOS_FOLDER}${org.image})`;
     listItem.className = "org";
 
-    listItem.setAttribute("title", `${org.name} (${org.country})`);
+    listItem.setAttribute("title", org.name);
     if (org.denounced) {
       listItem.dataset.denounced = "1";
     }
 
     const orgName = document.createElement("span");
-    orgName.innerHTML = `${org.name}<br />(${org.country})`;
+    orgName.textContent = org.name;
     orgName.className = "org-name";
 
     listItem.appendChild(orgName);
